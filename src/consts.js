9b028bd7e4fdb503d10ef8269a5eede873d26a81
dracz3 <daniel.racz@irob.uni-obuda.hu>
2021-04-19 20:21:48 +0200

    chg: separate env for dev + prod

--- src/consts.js
+++ src/consts.js
@@ -1,4 +1,13 @@
-export function get_full_ws_address(room, token, host) {
-  const WS_ADDRESS = `ws://${host}/ws`;
+var env = process.env.NODE_ENV || 'development';
+
+export function get_full_ws_address(room, token) {
+  let WS_ADDRESS;
+  if (env != 'development') {
+    WS_ADDRESS = `wss://${window.location.hostname}/ws`;
+  } else {
+    const PORT = 8000;
+    WS_ADDRESS = `ws://${window.location.hostname}:${PORT}/ws`;
+  }
+
   return `${WS_ADDRESS}/${room}/${token}`;
 }
