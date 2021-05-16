9aacf76aefa474efd6307d440014d44fdae79e86
dracz3 <daniel.racz@irob.uni-obuda.hu>
2021-05-16 10:03:22 +0200

    chg: make getenv a bit more smarter

--- src/getenv.js
+++ src/getenv.js
@@ -1,5 +1,12 @@
-var env = process.env.NODE_ENV || 'development';
-
+/**
+ * The main difference between prod and dev, is that
+ * prod env is behind https and a webserver (like Nginx)
+ *
+ * @returns True, if production config is detected.
+ */
 export function isProduction() {
-  return env !== 'production';
+  if (window.location.protocol != 'https:') {
+    return false;
+  }
+  return true;
 }
